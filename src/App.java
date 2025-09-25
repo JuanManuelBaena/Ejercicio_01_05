ed6ad6addb1677e9b4f4bb00c9791240c86d5b5a
Juan Manuel Baena <jbaeurb3009@g.educaand.es>
2025-09-25 21:04:17 +0200

    Feat: Ejercicio 5 coloreado

--- src/App.java
+++ src/App.java
@@ -8,6 +8,8 @@ public static final String AMARILLO = "\u001B[33m";
 public static final String AZUL = "\u001B[34m";
 public static final String PURPURA = "\u001B[35m";
 public static final String BLANCO = "\u001B[37m";
+public static final String CYAN = "\033[0;36m"; 
+
 
 // Códigos ANSI para colores de fondo
 public static final String FONDO_NEGRO = "\u001B[40m";
@@ -19,23 +21,23 @@ public static final String FONDO_PURPURA = "\u001B[45m";
 public static final String FONDO_CYAN = "\u001B[46m";
 public static final String FONDO_BLANCO = "\u001B[47m";
     public static void main(String[] args) throws Exception {
-String si = "SI";
-String prog = "PROG";
-String lm = "LM";
-String bd = "BD";
-String sos = "SOS";
-String ipe1 = "IPE1";
-String dig = "DIG";
-String ed = "ED";
+String si = ROJO+"SI"+RESET;
+String prog = AMARILLO+"PROG"+RESET;
+String lm = CYAN+"LM"+RESET;
+String bd = VERDE+"BD"+RESET;
+String sos = BLANCO+"SOS"+RESET;
+String ipe1 = PURPURA+"IPE1"+RESET;
+String dig = NEGRO+"DIG"+RESET;
+String ed = AZUL+"ED"+RESET;
 
 
 
         System.out.printf("%-10s%-10s%-10s%-10s%-10s%n", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes");
-        System.out.printf("%-10s%-10s%-10s%-10s%-10s%n", si, prog, si, ipe1, bd);
-        System.out.printf("%-10s%-10s%-10s%-10s%-10s%n", si, prog, si, ipe1, bd);
-        System.out.printf("%-10s%-10s%-10s%-10s%-10s%n", lm, prog, ipe1, prog, si);
-        System.out.printf("%-10s%-10s%-10s%-10s%-10s%n", lm, bd, dig, prog, ed);
-        System.out.printf("%-10s%-10s%-10s%-10s%-10s%n", bd, bd, prog, prog, ed);
-        System.out.printf("%-10s%-10s%-10s%-10s%-10s%n", bd, sos, prog, lm, ed);
+        System.out.printf("%-10s        %-10s      %-10s        %-10s      %-10s%n", si, prog, si, ipe1, bd);
+        System.out.printf("%-10s        %-10s      %-10s        %-10s      %-10s%n", si, prog, si, ipe1, bd);
+        System.out.printf("%-10s        %-10s      %-10s      %-10s      %-10s%n", lm, prog, ipe1, prog, si);
+        System.out.printf("%-10s        %-10s        %-10s       %-10s      %-10s%n", lm, bd, dig, prog, ed);
+        System.out.printf("%-10s        %-10s        %-10s      %-10s      %-10s%n", bd, bd, prog, prog, ed);
+        System.out.printf("%-10s        %-10s       %-10s      %-10s        %-10s%n", bd, sos, prog, lm, ed);
         }
 }
